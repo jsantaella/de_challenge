5acde07d5bf2cc5fb0b0f774e35114ae635c76b6
Jorge Santaella <jorgeasantaella@gmail.com>
2024-07-10 16:57:54 -0500

    Reorganización de importación de librarias de spark

--- pyspark_scripts/top_most_user_by_date.py
+++ pyspark_scripts/top_most_user_by_date.py
@@ -1,7 +1,10 @@
-from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
 from pyspark.sql.functions import *
+from pyspark.sql import SparkSession
+from pyspark.sql.window import Window
 from pyspark import SparkContext, SparkConf
+from pyspark.sql.functions import row_number
+from pyspark.sql.functions import get_json_object
 
 conf = SparkConf().setMaster("local[*]").setAppName("Dataframe_examples")
 sc = SparkContext(conf=conf)
