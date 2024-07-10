0914a6afab28fc7bd7d01d11d2dde038e6e97a0f
Jorge Santaella <jorgeasantaella@gmail.com>
2024-07-10 17:07:01 -0500

    Definición del primer select y cargue en dataframe del archivo de json

--- pyspark_scripts/top_most_user_by_date.py
+++ pyspark_scripts/top_most_user_by_date.py
@@ -6,7 +6,23 @@ from pyspark import SparkContext, SparkConf
 from pyspark.sql.functions import row_number
 from pyspark.sql.functions import get_json_object
 
-conf = SparkConf().setMaster("local[*]").setAppName("Dataframe_examples")
-sc = SparkContext(conf=conf)
+if __name__ == '__main__': 
 
-spark = SparkSession(sc)
+    #Para las pruebas se usar local[*] así utiliza todos los threads de la máquina en ambiente productivo se usa YARN. 
+    conf = SparkConf().setMaster("local[*]").setAppName("Dataframe_examples")
+    sc = SparkContext(conf=conf)
+
+    spark = SparkSession(sc)
+
+
+    #Lectura del archivo que contiene los JSON 
+    df = spark.read.text("farmers-protest-tweets-2021-2-4.json")
+    
+    """"
+    Para el primer ejercicio nos interesa hacer parse de las fechas y el username dado que queremos conocer 
+    la fecha con mayores trinos y el usuario que mas trino respectivamente
+    """
+    df = df.select(
+                get_json_object(df.value,"$.user.username").alias("username"),
+                get_json_object(df.value,"$.date").cast('date').alias("date"))\
+                .createOrReplaceTempView("dates_users")
