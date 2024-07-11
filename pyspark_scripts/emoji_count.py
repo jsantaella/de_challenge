c005a83a6d681162acfe7ed263635a6ba62fa3c2
Jorge Santaella <jorgeasantaella@gmail.com>
2024-07-11 15:46:22 -0500

    Inclusión de la session de Spark y aplicación de black linter

--- pyspark_scripts/emoji_count.py
+++ pyspark_scripts/emoji_count.py
@@ -1,16 +1,32 @@
-from pyspark.sql.types import ArrayType, StructType, StructField, StringType, IntegerType
+from pyspark.sql.types import (
+    ArrayType,
+    StructType,
+    StructField,
+    StringType,
+    IntegerType,
+)
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+from pyspark.sql.functions import *
+from pyspark import SparkContext, SparkConf
 import pyspark.sql.functions as fn
 import regex
 import emoji
 import re
 
-def find_all_emo(plain_text):
-  if plain_text is None:
-    return None
-  #emo_list = regex.findall(escape_list,plain_text)
-  return emoji.emoji_list(plain_text)
 
-if __name__ == '__main__': 
+def find_all_emo(plain_text):
+    if plain_text is None:
+        return None
+    # emo_list = regex.findall(escape_list,plain_text)
+    return emoji.emoji_list(plain_text)
+
+
+if __name__ == "__main__":
+
+    conf = SparkConf().setMaster("local[*]").setAppName("Dataframe_examples")
+    sc = SparkContext(conf=conf)
+    spark = SparkSession(sc)
 
     search_all_emojis = fn.udf(lambda y: find_all_emo(y), ArrayType(StringType()))
 
@@ -23,5 +39,6 @@ if __name__ == '__main__':
     Resultados intermedios: 
     Col : emoji_in_post: [{'match_start': 85, 'match_end': 86, 'emoji': '🙄'}, {'match_start': 86, 'match_end': 87, 'emoji': '🙄'}]
     """
-    df = df.select(fn.get_json_object(df.value,"$.content").alias('content'))\
-        .withColumn("emoji_in_post", search_all_emojis(fn.col("content")))
+    df = df.select(
+        fn.get_json_object(df.value, "$.content").alias("content")
+    ).withColumn("emoji_in_post", search_all_emojis(fn.col("content")))
