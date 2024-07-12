895b3ace9dbc7f492439003e449f49b678c2874c
Jorge Santaella <jorgeasantaella@gmail.com>
2024-07-11 23:59:33 -0500

    Creacióon del workflow de dataproc para ejecucion de un job de spark

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,3 +40,50 @@ resource "google_storage_bucket_object" "function_code" {
   bucket = module.bucket.name
   source = data.archive_file.function_code.output_path
 }
+
+resource "google_storage_bucket_object" "top_most_user_by_date" {
+  name   = "top_most_user_by_date.py"
+  source = "../pyspark_scripts/code/top_most_user_by_date.py"
+  bucket = module.bucket.name
+}
+
+resource "google_dataproc_workflow_template" "template" {
+  name     = "${var.project_id}-dataproc-workflow"
+  location = "us-west1"
+  placement {
+    managed_cluster {
+      cluster_name = "${var.project_id}-my-cluster"
+      config {
+        gce_cluster_config {
+          zone = "${var.location}-a"
+          tags = ["foo", "bar"]
+        }
+        master_config {
+          num_instances = 1
+          machine_type  = "n2-standard-2"
+          disk_config {
+            boot_disk_type    = "pd-ssd"
+            boot_disk_size_gb = 30
+          }
+        }
+        worker_config {
+          num_instances = 2
+          machine_type  = "n1-standard-2"
+          disk_config {
+            boot_disk_size_gb = 30
+            num_local_ssds    = 2
+          }
+        }
+        software_config {
+          image_version = "2.0.35-debian10"
+        }
+      }
+    }
+  }
+  jobs {
+    step_id = "PysparkJob"
+    pyspark_job {
+      main_python_file_uri = join("/",["gs://${module.bucket.name}", google_storage_bucket_object.top_most_user_by_date.output_name])
+    }
+  }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
-variable "project_id"{
-    type = string 
-    default = "grand-sweep-326023"
+variable "project_id" {
+  type    = string
+  default = "grand-sweep-326023"
 }
 
-variable "location"{
-    type = string 
-    default = "us-west1"
+variable "location" {
+  type    = string
+  default = "us-west1"
 }
