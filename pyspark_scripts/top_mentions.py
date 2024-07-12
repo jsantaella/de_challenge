75ffffa8bb93e23169fa579f13069ed156c110f8
Jorge Santaella <jorgeasantaella@gmail.com>
2024-07-11 21:59:03 -0500

    Primera lectura y extracción de datos de Json del archivo

--- pyspark_scripts/top_mentions.py
+++ pyspark_scripts/top_mentions.py
@@ -23,3 +23,21 @@ if __name__ == "__main__":
     # Lectura del archivo de trinos en memoria de Spark
 
     df = spark.read.text("farmers-protest-tweets-2021-2-4.json")
+
+    #Esquema de ArrayType para los registros que contiene el mentionedUsers, de esta forma solo extraemos los username que interesan
+    user_schema = ArrayType(
+        StructType([
+            StructField("username", StringType(), True),
+        ])
+    )
+
+    """
+    Inicio del procesamiento del dataset
+    1. Se leen los archivos json y se extrae el key 'mentionedUsers' este contiene las menciones del objeto trino
+    2. Se procesa el mentionedUsers y se convierte en listas de nombres de usuario
+    """
+    df = df.withColumn(
+        "username_mentions", fn.get_json_object(df.value, "$.mentionedUsers")
+    ).withColumn(
+        "username_mentions_list", from_json(col("username_mentions"), user_schema)
+    )
